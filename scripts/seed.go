7be8c3f1e2bc8256f03679b5f163893101a21ccd
Jorge Rojas <jorge.otto.415@gmail.com>
2023-08-18 21:07:23 -0500

    add seed user

--- scripts/seed.go
+++ scripts/seed.go
@@ -16,6 +16,7 @@ var (
 	client     *mongo.Client
 	hotelStore *db.MongoHotelStore
 	roomStore  *db.MongoRoomStore
+	userStore  *db.MongoUserStore
 	ctx        = context.Background()
 )
 
@@ -56,10 +57,30 @@ func seedHotel(name, location string, rating, priceCategory float64) {
 	}
 }
 
+func seedUser(name, lName, email, password string) {
+	newUser := types.NewUserParams{
+		FirstName: name,
+		LastName:  lName,
+		Email:     email,
+		Password:  password,
+	}
+	if errors := newUser.Validate(); len(errors) != 0 {
+		log.Fatal(errors)
+	}
+	user, err := types.NewUserFromParams(&newUser)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := userStore.InsertUser(ctx, user); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 	seedHotel("The Coffin", "Transylvania", 3.5, 1)
 	seedHotel("Yokai Inn", "Japan", 4.2, 2)
 	seedHotel("Sherlock hideout", "London", 4.9, 2.5)
+	seedUser("Jorge", "Rojas", "jorge.otto.415@gmail.com", "Jrojas1234$")
 }
 
 func init() {
@@ -77,4 +98,5 @@ func init() {
 
 	hotelStore = db.NewMongoHotelStore(client, db.DBNAME)
 	roomStore = db.NewMongoRoomStore(client, hotelStore)
+	userStore = db.NewMongoUserStore(client, db.DBNAME)
 }
